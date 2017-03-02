c47e262e3b969eef4531e7da4a5778e31c2f1c8d
Itay Greenspon <itay@maxeler.com>
2017-03-02 15:36:59 +0000

    Re-write of stock.py, nicer and better.

--- src/stock.py
+++ src/stock.py
@@ -3,23 +3,25 @@
 Market data generator script.
 """
 
-import sys
-import random
-import warnings
 import argparse
-from collections import namedtuple
-from collections import deque
+import bisect
+import csv
+import random
+import sys
+import time
+import warnings
+from enum import Enum
+from functools import reduce
 
 import numpy
 from matplotlib import pyplot
-
+from recordclass import recordclass
+from sortedcontainers import SortedListWithKey
 
 # all new resting orders will be placed qith qty 1
 # book is kept with at least one level on bid and offer
 
-MIN_SPREAD = 1
 DECIMALS = 1
-MIN_DEPTH = 5
 ORDER_QUANTITY = 1
 ERROR_MARGIN = 0.01
 
@@ -28,222 +30,187 @@ def match(price1, price2):
     """Checks if prices are the same."""
     return abs(price1 - price2) < ERROR_MARGIN
 
+def now_nanos():
+    """Returns the current time since epoch in nanoseconds"""
+    return int(time.time() * 1000000000)
 
-Level = namedtuple('Level', ['price', 'quantity'])
+class Side(Enum):
+    """
+        Trading Side
+    """
+    BUY = 1
+    SELL = 2
 
+OrderBookLevel = recordclass('OrderBookLevel',
+                             ['price', 'qty', 'order_count'])
+Order = recordclass('Order', ['secid', 'side', 'price', 'qty'])
+Trade = recordclass('Trade', ['price', 'qty', 'aggressor'])
 
-class Side(object):
-
-    def __init__(self, incrementing, debug):
-        self.levels = deque()
-        self.incrementing = incrementing
-        self.name = "BID"
-        if self.incrementing:
-            self.name = "OFFER"
-        self.operation = "SELL"
-        if self.incrementing:
-            self.operation = "BUY"
-        self.debug = debug
-
-    def execute(self, price):
-        if len(self.levels) > 0:
-            if self.incrementing:  # OFFER
-                if price >= self.levels[0].price - ERROR_MARGIN:
-                    if self.debug:
-                        print("%s for %f at level %f" %
-                              (self.operation, price, self.levels[0].price))
-                    self.levels.popleft()
-                    return True
-                return False
-            else:
-                if price <= self.levels[0].price + ERROR_MARGIN:
-                    if self.debug:
-                        print("%s for %f at level %f" %
-                              (self.operation, price, self.levels[0].price))
-                    self.levels.popleft()
-                    return True
-                return False
-        else:
-            return False
-
-    def better(self, price):
-        if self.incrementing:
-            return price > self.levels[0].price
-        else:
-            return price < self.levels[0].price
-
-    def add(self, price):
-        price = round(price, DECIMALS)
-        if self.debug:
-            print("Add to %s: %f" % (self.name, price))
-
-        if len(self.levels) == 0:
-            self.levels.append(Level(price, ORDER_QUANTITY))
-            return
-        out_queue = deque()
-
-        added = False
-        for level in self.levels:
-            if added is False:
-                if match(price, level.price):
-                    out_queue.append(
-                        Level(level.price, level.quantity + ORDER_QUANTITY))
-                    added = True
-                elif self.incrementing:  # OFFER
-                    if price < level.price:
-                        out_queue.append(Level(price, ORDER_QUANTITY))
-                        added = True
-                    out_queue.append(level)
-                else:
-                    if price > level.price:
-                        out_queue.append(Level(price, ORDER_QUANTITY))
-                        added = True
-                    out_queue.append(level)
-            else:
-                out_queue.append(level)
-        if added is False:
-            out_queue.append(Level(price, ORDER_QUANTITY))
-        self.levels = out_queue
-
-    def depth(self):
-        return len(self.levels)
-
-    def top(self):
-        return self.levels[0]
-
-    def to_str(self, index):
-        if index < self.depth():
-            if self.incrementing:
-                return "%f,%u,1" % (self.levels[index].price, self.levels[index].quantity)
-            else:
-                return "1,%u,%f" % (self.levels[index].quantity, self.levels[index].price)
-        else:
-            return "0,0,0"
-
-
-class Book(object):
-
-    def __init__(self, outputfile, security, debug):
-        self.bid = Side(False, debug)
-        self.offer = Side(True, debug)
-        self.file = outputfile
-        self.file.write(
-            "Security, bid valid, bid qty, bid px, "
-            "ask px, ask qty, ask valid, "
-            "Timestamp, execSummary, Price, "
-            "DisplayQuantity, AggressorSide(1-buy,2-sell)\n")
+class OrderBook(object):
+    """An Order Book data model"""
+    def __init__(self, security):
+        self.bid = SortedListWithKey(key=(lambda level: level.price))
+        self.offer = SortedListWithKey(key=(lambda level: level.price))
         self.security = security
-        self.timestamp = 100000000
-        self.debug = debug
-        self.timeseries = []
 
-    def incTime(self, big_step):
-        if big_step:
-            self.timestamp = self.timestamp + 100000000
-        else:
-            self.timestamp = self.timestamp + 1000000
+    def cross(self, bid, offer):
+        """
+            Check if two levels can cross
+        """
+        if bid.price >= offer.price:
+            remaining_qty = offer.qty - bid.qty
+            return (True, remaining_qty)
 
-    def buy(self, price, doUpdate=True):
-        self.incTime(False)
-        qty = 0
-        if self.offer.depth() > 0:
-            qty = self.offer.levels[0].quantity
+        return (False, 0)
 
-        if not self.offer.execute(price):
-            self.bid.add(price)
-            if doUpdate:
-                self.write_top(0, 0, 0, 0)
-        else:
-            self.write_top(1, price, qty, 1)
+    def match(self, aggressor_side):
+        """
+            Match orders and return a list of trades
+        """
 
-    def sell(self, price, doUpdate=True):
-        self.incTime(False)
-        qty = 0
-        if self.bid.depth() > 0:
-            qty = self.bid.levels[0].quantity
+        # print("Matching on the following book:")
+        # self.print()
+        trades = []
+        for bid_i in range(len(self.bid) - 1, -1, -1):
+            bid = self.bid[bid_i]
+            size_offer = len(self.offer)
+            offer_i = 0
+            while offer_i < size_offer:
+                offer = self.offer[offer_i]
+                (crossed, remaining_qty) = self.cross(bid, offer)
+                if crossed:
+                    trade = Trade(price=offer.price, qty=offer.qty, aggressor=aggressor_side)
+                    stop = False
+                    if remaining_qty >= 0:
+                        offer.qty = remaining_qty
+                        trade.qty = bid.qty
+                        del self.bid[bid_i]
+                        stop = True
+                    if remaining_qty <= 0:
+                        bid.qty = abs(remaining_qty)
+                        del self.offer[offer_i]
+                        size_offer -= 1
+                    else:
+                        offer_i += 1
+                    trades += [trade]
+                    if stop:
+                        break
+                else:
+                    return trades
+        return trades
 
-        if not self.bid.execute(price):
-            self.offer.add(price)
-            if doUpdate:
-                self.write_top(0, 0, 0, 0)
-        else:
-            self.write_top(1, price, qty, 2)
 
-    def addToChange(self, midDiff):
-        if midDiff > 0:
-            self.buy(self.bid.top().price + midDiff)
-        elif midDiff < 0:
-            self.sell(self.offer.top().price + midDiff)
+    def compact(self, levels):
+        """
+            Compacts an order list, such that orders at the same price level are merged.
+            This assumes the order list is sorted.
+        """
 
-    def executeToChange(self, midDiff):
-        if midDiff > 0:
-            top = self.offer.top().price
-            if self.offer.depth() < MIN_DEPTH:
-                self.sell(top + 2 * midDiff, False)
-            self.buy(top + midDiff)
-        elif midDiff < 0:
-            top = self.bid.top().price
-            if self.bid.depth() < MIN_DEPTH:
-                self.buy(top + 2 * midDiff, False)
-            self.sell(top + midDiff)
-
-    def move_mid(self, target):
-        """Move mid price"""
-        if self.debug:
-            print("moving mid from %f to %f" % (self.mid(), target))
-        loops = 0
-        while match(target, self.mid()) is False:
-            mid = self.mid()
-            spread = self.spread()
-            if self.debug:
-                print("current mid %f target mid %f spread%f" %
-                      (mid, target, spread))
-            diff = target - mid
-            if diff < spread / 1.5 and spread > MIN_SPREAD:
-                self.addToChange(2 * diff)
+        # print("Compacting book")
+        # self.print()
+        last_level = None
+        for i in range(len(levels) - 1, -1, -1):
+            level = levels[i]
+            if last_level:
+                if level.price == last_level.price:
+                    last_level.qty += level.qty
+                    last_level.order_count += level.order_count
+                    del levels[i]
+                else:
+                    last_level = level
             else:
-                self.executeToChange(diff)
-            loops = loops + 1
-            if loops > 10:
-                warnings.warn("Could not reach target mid")
-                break
-        self.timeseries += [self.mid()]
+                last_level = level
 
-        if self.debug:
-            print("mid is now %f" % self.mid())
-            self.prints()
-            print("---")
+    def order(self, order):
+        """
+            Apply an order to the Order Book.
+            Return a list of trades generated as a result.
+        """
+
+        print("Evaluating order: ", order)
+        if self.security != order.secid:
+            raise ("Cannot place order for security "
+                   "%s on book[%s]" % (order.security, self.security))
+
+        levels = self.bid
+        if order.side == Side.SELL:
+            levels = self.offer
+
+        levels.add(OrderBookLevel(price=order.price, qty=order.qty, order_count=1))
+        self.compact(levels)
+        return self.match(order.side)
 
     def mid(self):
-        if self.bid.depth() > 0 and self.offer.depth() > 0:
-            return (self.bid.top().price + self.offer.top().price) / 2
-        else:
-            raise IndexError("Single sided book (mid)")
+        """
+            Get mid price of top of Book
+        """
+        if self.bid and self.offer:
+            return (self.bid[-1].price + self.offer[0].price) / 2.0
 
-    def spread(self):
-        if self.bid.depth() > 0 and self.offer.depth() > 0:
-            return self.offer.top().price - self.bid.top().price
-        else:
-            raise IndexError("Single sided book (spread)")
+        return 0
 
-    def write_top(self, isExec, price, quantity, side):
-        """Writes the Top of Book to a file"""
-        # security, bid valid, bid qty, bid px, offerpx, offer qty, offer
-        # valid, timestamp, execSummary, price, qty, side
-        self.file.write("%d,%s,%s,%d,%d,%f,%u,%d\n" %
-                        (self.security, self.bid.to_str(0), self.offer.to_str(0),
-                         self.timestamp, isExec, price, quantity, side))
+    def reduce_order_count(self, level1, level2):
+        """
+            Reduction function for order_counts
+        """
+        count1 = level1
+        count2 = level2
+        if isinstance(level1, OrderBookLevel):
+            count1 = level1.order_count
+        if isinstance(level2, OrderBookLevel):
+            count2 = level2.order_count
+        return count1 + count2
 
-    def prints(self):
-        max_len = self.bid.depth()
-        if self.offer.depth() > max_len:
-            max_len = self.offer.depth()
-        for i in range(0, max_len):
-            print("[%s | %s]" % (self.bid.to_str(i), self.offer.to_str(i)))
-    def get_series(self):
-        return self.timeseries
+    def num_orders(self, levels):
+        """
+            Computes the number of open orders on a list of book levels
+        """
+        return reduce(self.reduce_order_count, levels)
 
-def gen_mid(config):
+    def num_offers(self):
+        """
+            Returns the number of offers on the book
+        """
+        return self.num_orders(self.offer)
+
+    def num_bids(self):
+        """
+            Returns the number of bids on the book
+        """
+        return self.num_orders(self.bid)
+
+    def best_bid(self):
+        """Get best bid"""
+        if self.bid:
+            return self.bid[-1]
+        return None
+
+    def best_offer(self):
+        """Get best offer"""
+        if self.offer:
+            return self.offer[0]
+        return None
+
+    def print(self):
+        """
+            Prints the order book
+        """
+        size_bid = len(self.bid)
+        size_offer = len(self.offer)
+        print("Book[%s]: %d bids, %d offers --> mid @ %f"  % (self.security,
+                                                              size_bid, size_offer, self.mid()))
+        print("{0: ^32} | {1: ^32}".format("bid", "offer"))
+        print("{0:^10},{1:^10},{2:^10} | {3:^10}, {4:^10}, {5:^10}".format(
+            "count", "qty", "price", "price", "qty", "count"))
+
+        empty_level = OrderBookLevel("-", "-", "-")
+        for i in range(max(size_bid, size_offer)):
+            bid = self.bid[-(i+1)] if i < size_bid else empty_level
+            offer = self.offer[i] if i < size_offer else empty_level
+            print("{0:^10},{1:^10},{2:^10} | {3:^10}, {4:^10}, {5:^10}".format(
+                bid.order_count, bid.qty, bid.price, offer.price, offer.qty, offer.order_count))
+
+def gen_orders(config):
     """Generator function for the mid price"""
     upper_bound = config.base + config.bound
     lower_bound = config.base - config.bound
@@ -271,29 +238,54 @@ def gen_mid(config):
             mid = upper_bound
 
         mid = round(mid, DECIMALS)
-        yield mid
+        qty = random.randint(1, 10)
+
+        yield (mid, qty)
 
 def gen_book(config):
     """Generate order book"""
+    print("Using random seed: ", config.randseed)
     random.seed(config.randseed)
-    ideal_mid = []
-    book_mid = []
 
-    book = Book(config.outputfile, config.instrument, config.debug)
+    book = OrderBook(security=config.instrument)
 
     # Initialize book
-    mid = config.base
     for i in range(config.depth):
-        book.buy(mid - (i+1) * MIN_SPREAD)
-        book.sell(mid + (i+1) * MIN_SPREAD)
+        delta = (i+1) * 1.0
+        book.order(Order(secid=config.instrument,
+                         side=Side.BUY, price=(config.base - delta), qty=1))
+        book.order(Order(secid=config.instrument,
+                         side=Side.SELL, price=(config.base + delta), qty=1))
 
-    for mid in gen_mid(config):
-        book_mid += [book.mid()]
-        ideal_mid += [mid]
-        book.move_mid(mid)
-        book.incTime(True)
+    bid_series = []
+    offer_series = []
+    mid_series = []
+    for (mid, qty) in gen_orders(config):
+        # book.print()
+        order = None
 
-    return book
+        num_offers = book.num_offers()
+        num_bids = book.num_bids()
+        side = Side.BUY
+        # if direction > 0:
+        if num_offers > num_bids:
+            side = Side.BUY
+        elif num_offers < num_bids:
+            side = Side.SELL
+        else:
+            side = Side.SELL if random.randint(0, 1) == 0 else Side.BUY
+
+        order = Order(secid=config.instrument,
+                      side=side, price=mid, qty=qty)
+        trades = book.order(order)
+        if trades:
+            print("New trades: ", trades)
+        mid_series += [book.mid()]
+        bid_series += [book.best_bid().price]
+        offer_series += [book.best_offer().price]
+
+    book.print()
+    return (bid_series, offer_series, mid_series)
 
 def parse_args(argv):
     """Parse command line arguments"""
@@ -328,7 +320,7 @@ def parse_args(argv):
 
     parser.add_argument("-r", "--randomseed", dest="randseed",
                         help="Random seed",
-                        action="store", type=int, default=1)
+                        action="store", type=int, default=2)
 
     parser.add_argument("-o", "--output", dest="outputfile",
                         help="Output file name",
@@ -342,12 +334,11 @@ def main(argv):
 
     args = parse_args(argv)
 
-    book = gen_book(args)
+    (bid_series, offer_series, mid_series) = gen_book(args)
 
-    print("Final book")
-    book.prints()
-
-    pyplot.plot(range(args.samples), numpy.array(book.get_series()), 'b')
+    pyplot.plot(range(args.samples), numpy.array(mid_series), 'b')
+    pyplot.plot(range(args.samples), numpy.array(bid_series), 'g')
+    pyplot.plot(range(args.samples), numpy.array(offer_series), 'r')
     pyplot.show()
 
 
